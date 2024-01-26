93d3c7faea254be04efcff4ec1547dc27f5352fb
林炘秀 <jenny031054@gmail.com>
2024-01-26 23:44:23 +0800

    update

---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ text }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+export default{
+  data(){
+    return {
+      text:{},
+
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_PATH);
+    const url = import.meta.env.VITE_PATH
+    this.$http.get(url)
+    .then((res)=>{
+      console.log(res);
+      this.text = res.data.results[0]
+
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+}
+</script>
